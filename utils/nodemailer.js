eb387d4d7d7dab4da02875b77b1d4817053090f1
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-21 20:50:59 +0100

    created a file and functions that will handle mailing requests sent to the backend

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -18,18 +18,18 @@ const styling = `
     p { color: #151515; }
 `;
 
-const createOptions = ({ name, email, subject, text }) => ({
+const createOptions = ({ name, email, subject }) => ({
     from: process.env.EMAIL,
     to: 'emmanuelezema6@gmail.com',
-    subject: `${subject} (${name})`,
-    text: `From: ${name} - ${email}\n Message: ${text}`,
+    subject: `${subject.length > 20 ? subject.substring(0, 20) + '...' : subject} (${name})`,
+    text: `From: ${name} - ${email}`,
     html: `
             <html>
                 <head>
                     <style>${styling}</style>
                 </head>
-                <h2>Message from <span>${text}</span> - <span>${email}</span></h2>
-                <p>${text}<p>
+                <h2>Message from <span>${name}</span> - <span>${email}</span></h2>
+                <p>${subject}<p>
             </html>
         `
 });
