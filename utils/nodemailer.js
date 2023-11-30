81612ea0d5e8579b1e5a450655c35696738e6f26
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-30 23:18:03 +0100

    Added helper functions for mail.js

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -1,7 +1,8 @@
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
-
 dotenv.config();
+
+const MAIL_STYLING = '* { list-style: none; } h1 { font-weight: normal; } h2 span { font-weight: bold; } p { color: #151515; }';
 
 const transporter = nodemailer.createTransport({
     service: "hotmail",
@@ -11,27 +12,48 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-const styling = `
-    * { list-style: none; }
-    h1 { font-weight: normal; }
-    h2 span { font-weight: bold; }
-    p { color: #151515; }
-`;
+function validateMailValues(valuesArray) {
+    let failed = false, message = '';
+    try {
+        if (!valuesArray) throw new Error('No values array found');
+        for (let [key, value] of valuesArray) {
+            if (!value || value?.length < 1) throw new Error(`${key} field cannot be left empty`);
+        }
+    } catch (error) {
+        message = error.message;
+        failed = true;
+    }
+    return { failed, message };
+};
 
-const createOptions = ({ name, email, subject }) => ({
-    from: process.env.EMAIL,
-    to: 'emmanuelezema6@gmail.com',
-    subject: `${subject.length > 20 ? subject.substring(0, 20) + '...' : subject} (${name})`,
-    text: `From: ${name} - ${email}`,
-    html: `
+function convertValuesArrayToObject(objectEntries) {
+    let newMailValues = {};
+    for (let entry of objectEntries) {
+        if (entry?.length !== 2) continue;
+        let [key, value] = entry;
+        if (typeof value !== 'string') newMailValues[key] = JSON.stringify(value);
+        else newMailValues[key] = value;
+    }
+    return newMailValues;
+}
+
+function createOptions({ name, email, subject }) {
+    if (!name || !email || !subject) return null;
+    return {
+            from: process.env.EMAIL,
+            to: 'emmanuelezema6@gmail.com',
+            subject: `${subject.length > 20 ? subject.substring(0, 20) + '...' : subject} (${name})`,
+            text: `From: ${name} - ${email}`,
+            html: `
             <html>
                 <head>
-                    <style>${styling}</style>
+                    <style>${MAIL_STYLING}</style>
                 </head>
                 <h2>Message from <span>${name}</span> - <span>${email}</span></h2>
                 <p>${subject}<p>
             </html>
         `
-});
+    }
+}
 
-module.exports = { transporter, createOptions }
+module.exports = { transporter, validateMailValues, convertValuesArrayToObject, createOptions }
