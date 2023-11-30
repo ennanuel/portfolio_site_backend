d54e262d566f399c5edbc7ca780605b6155df495
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-11-30 23:15:45 +0100

    Added route that handles mailing from frontend

--- routes/mail.js
+++ routes/mail.js
@@ -1,17 +1,19 @@
 const router = (require('express')).Router();
-const { transporter, createOptions } = require('../utils/nodemailer');
+const { transporter, createOptions, validateMailValues, convertValuesArrayToObject } = require('../utils/nodemailer');
 
 router.post('/', async function (req, res) {
-    const { subject, email, phone, name } = req.body;
-    if (!subject || !phone || !email || !name) return res.status(500).json({ message: 'Required fields cannot be empty ' });
+    const mailContentArray = Object.entries(req.body);
+    const { failed, message } = validateMailValues(mailContentArray);
+    if (failed) return res.status(500).json({ message });
+    const { name, email, subject, phone } = convertValuesArrayToObject(mailContentArray);
     const options = createOptions({ name, email, subject, phone });
     transporter.sendMail(options, (error, info) => {
         if(error) {
-            console.log(error)
-            return res.status(500).json({success: false})
+            console.log(error);
+            return res.status(500).json({ success: false });
         } else {
-            console.log(info.response)
-            return res.status(200).json({success: true})
+            console.log(info.response);
+            return res.status(200).json({ success: true });
         }
     })
 })
