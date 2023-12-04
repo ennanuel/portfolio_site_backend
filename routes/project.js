62c2b0f466d201fee0f85e18e16194f9f5a83277
Emmanuel Ezema <emmanuelezema6@gmail.com>
2023-12-04 01:17:24 +0100

    Resolved fetch projects logic

--- routes/project.js
+++ routes/project.js
@@ -1,14 +1,14 @@
 const router = (require('express')).Router();
 const axios = require('axios');
 const { uploadMiddleware } = require('../utils/upload');
-const projects = require('../views/portfolio-projects.json');
 
 router.get('/page/add', (req, res) => res.render('add'));
 router.get('/page/edit', (req, res) => res.render('edit'));
 
 router.get('/', async function (req, res) {
     try {
-        // const projects = await axios.get(process.env.FIREBASE_DB_URL);
+        const result = await axios.get(process.env.FIREBASE_DB_URL);
+        const projects = result.data;
         return res.status(200).json(projects);
     } catch (error) {
         console.error(error);
@@ -23,7 +23,7 @@ router.post('/upload', uploadMiddleware, async function (req, res) {
         const previousProjects = result.data || [];
         const newValues = [...previousProjects, project];
         await axios.put(process.env.FIREBASE_DB_URL, newValues);
-        res.send('Project uplaoded');
+        res.send('Project uploaded');
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Could not upload project' });
